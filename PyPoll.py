9cb177eb9330d568c60aa8efdad9b26529b837df
yessiez <jess.margaret.sanchez@gmail.com>
2021-05-09 00:20:26 -0400

    Get the Candidates in the Election.

--- PyPoll.py
+++ PyPoll.py
@@ -23,22 +23,39 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 # 1. Initialize a total vote counter
 total_votes = 0
 
+# Declare a new list
+candidate_options = []
+
 # Open the election results and read the file, using the with statement to read the file.
 with open(file_to_load) as election_data:
     # To do: Read the data and perform analysis here.
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
 
     # Print each row in the CSV file.
     for row in file_reader:
         # 2. Add to the total vote count.
         total_votes += 1
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # Add the candidate name to the candidate list.
+        # candidate_options.append(candidate_name)
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+
+# Print the candidate list.
+print(candidate_options)
 
 # 3. Print the toal votes.
-print(total_votes)
+# print(total_votes)
 
 
 # Using the with statement, open the file as a text file.
