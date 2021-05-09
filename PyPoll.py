582804d3640add6b7a13c1500862c1e7b9df1d73
yessiez <jess.margaret.sanchez@gmail.com>
2021-05-09 00:46:09 -0400

    Determine Candidates' Percentage of Votes

--- PyPoll.py
+++ PyPoll.py
@@ -58,8 +58,18 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
 
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+
 # Print the candidate vote dictionary.
-print(candidate_votes)
+# print(candidate_votes)
 
 # Print the candidate list.
 # print(candidate_options)
