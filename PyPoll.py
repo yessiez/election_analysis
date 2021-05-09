664d56466a5b8b2eb8f1782b4e9c5102cb58b041
yessiez <jess.margaret.sanchez@gmail.com>
2021-05-08 23:59:43 -0400

    Adding PyPoll filewith total vote count.

--- PyPoll.py
+++ PyPoll.py
@@ -20,32 +20,40 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Open the file, using the with statement to read the file.
+# 1. Initialize a total vote counter
+total_votes = 0
+
+# Open the election results and read the file, using the with statement to read the file.
 with open(file_to_load) as election_data:
     # To do: Read the data and perform analysis here.
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
+
     # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
+
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+        # 2. Add to the total vote count.
+        total_votes += 1
+
+# 3. Print the toal votes.
+print(total_votes)
 
 
 # Using the with statement, open the file as a text file.
 # with open(file_to_save, "w") as txt_file:
 
-    ##txt_file.write("Counties in the Election\n--------------------------\n")
+##txt_file.write("Counties in the Election\n--------------------------\n")
 
-    # 1 way: Write three counties to the file.
-    # txt_file.write("Arapahoe, ")
-    # txt_file.write("Denver, ")
-    # txt_file.write("Jefferson")
+# 1 way: Write three counties to the file.
+# txt_file.write("Arapahoe, ")
+# txt_file.write("Denver, ")
+# txt_file.write("Jefferson")
 
-    # 2nd way: Write three counties to the file.
-    # txt_file.write("Arapahoe, Denver, Jefferson")
+# 2nd way: Write three counties to the file.
+# txt_file.write("Arapahoe, Denver, Jefferson")
 
-    # 3rd way: If we want to write each county on a separate line, we need to add the newline escape sequence to the end of each county name.
-    # The newline escape sequence is the letter "n" preceded by the backward slash like this: \n.
-    # txt_file.write("Arapahoe\nDenver\nJefferson")
+# 3rd way: If we want to write each county on a separate line, we need to add the newline escape sequence to the end of each county name.
+# The newline escape sequence is the letter "n" preceded by the backward slash like this: \n.
+# txt_file.write("Arapahoe\nDenver\nJefferson")
